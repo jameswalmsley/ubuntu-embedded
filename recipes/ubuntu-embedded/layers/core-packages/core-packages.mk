6955a6725ec3a2eee491d4c82b283bd5c1b37687
James Walmsley <james@fullfat-fs.co.uk>
2025-04-03 11:20:44 +0100

    simplify core-packages

--- recipes/ubuntu-embedded/layers/core-packages/core-packages.mk
+++ recipes/ubuntu-embedded/layers/core-packages/core-packages.mk
@@ -8,10 +8,6 @@ $(L) += $(core-packages)
 include $(BUILD_LAYER)
 
 $(core-packages):
-	mkdir -p $(rootfsdir)/overlay
-	mount --bind $(basedir)/overlay $(rootfsdir)/overlay
 	arch-chroot $(rootfsdir) /overlay/core-packages.sh
-	umount -R $(rootfsdir)/overlay
 	$(stamp)
-
 
